e0e7f2ba583c32db4778ab53bc6eecb5030cdf4b
theawssemdoudd <143346166+theawssemdoudd@users.noreply.github.com>
2025-09-05 00:23:33 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,24 +1,22 @@
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: 'Telegram Mini App',
-  description: 'A simple Telegram Mini App using Next.js 14'
+export const metadata = {
+  title: "Telegram Mini App",
+  description: "A simple Telegram Mini App using Next.js 14",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <Script src="https://telegram.org/js/telegram-web-app.js" strategy="beforeInteractive" />
+        <Script
+          src="https://telegram.org/js/telegram-web-app.js"
+          strategy="beforeInteractive"
+        />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
